4ef5f494df20f9ace0e1543903bf23b1767cd23b
Onbashi-Meriem <meryemonbasi5@gmail.com>
2022-06-21 12:32:05 +0200

    ng-invalid, ng-valid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'AngularForms';
+  
+
+  onSubmit(form:NgForm) {
+    if(form.valid){
+      console.log(form);
+    }
+    else{
+      alert('form da eksik bilgiler var')
+    }
+    
+  }
 }
